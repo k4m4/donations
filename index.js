401b935820f498b9de81851fe379c5415224ceaa
blackout314 <Viaggiare04>
2017-09-28 10:08:54 +0200

    move index.html on assets and rename output.html

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 prompt.start();
 
 prompt.get(['bitcoin_address', 'paypal_me_username', 'analytics_tracking_id'], function (err, result) {
-  fs.readFile( __dirname + '/index.html', function (err, data) {
+  fs.readFile( __dirname + '/assets/index.html', function (err, data) {
     if (err) {
       throw err; 
     }
@@ -13,7 +13,7 @@ prompt.get(['bitcoin_address', 'paypal_me_username', 'analytics_tracking_id'], f
     var html = html.replace(/{{BITCOIN}}/g, result.bitcoin_address);
     var html = html.replace(/{{ANALYTICS}}/g, result.analytics_tracking_id);
 
-    fs.writeFile("output.html", html, function(err) {
+    fs.writeFile("index.html", html, function(err) {
       if(err) {
         return console.log(err);
       }
